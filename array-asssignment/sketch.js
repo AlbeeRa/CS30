7fafdab558d3944a921516fa61b47ed38f724abb
Albee <ar.senpai03@gmail.com>
2023-03-15 15:18:14 -0600

    good

--- array-asssignment/sketch.js
+++ array-asssignment/sketch.js
@@ -1,15 +1,33 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// Generative Art [[Messing around]]
+// March 3rd, 2023
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background ("white");
+  lotsLine(100,100);
 }
 
 function draw() {
-  background(220);
+}
+function lotsLine(cols,rows){
+  for (let x = 0; x< cols; x++){
+    for (let y = 0; y < rows; y++){
+      let spaceAmount = width/cols;
+      diagonalLine(x*spaceAmount,y*spaceAmount,spaceAmount);
+    }
+  }
+}
+
+function diagonalLine(x,y,spaceline){
+  if (random(100) > 50) {
+  //postive slope
+    fill(random(255),random(255),random(255));
+    rect(x - spaceline/2, y + spaceline/2, x + spaceline/2, y - spaceline/2);
+  }
+  else {
+    //negative slope
+    fill(random(255),random(255),random(255));
+    rect(x - spaceline/2, y - spaceline/2, x + spaceline/2, y + spaceline/2);
+  }
+
 }
