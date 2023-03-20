bcc3e80d048ce9a6f55ea15fac82657f6b0cdaf6
AlbeeRa <ar.senpai03@gmail.com>
2023-03-20 00:56:49 -0600

    done

--- array-asssignment/sketch.js
+++ array-asssignment/sketch.js
@@ -1,20 +1,40 @@
-// Generative Art [[Messing around]]
-// March 3rd, 2023
+// Albee Raida
+// Assignment: Generative Art [Rainbow sphereralization]
+// March 14th, 2023
 
-let bubbles = [];
+let nums = [100,25,46,72];
+let fr = 20; //starting FPS
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
+  frameRate(fr);
+  createCanvas(windowWidth,windowHeight); 
 }
 
 function draw() {
-  spawn()
+  background(0);
+  //background(random(255)); //[will hurt your eyes if you stare for too long.]
+  //circles();
+  fillCircle();
+}
+function circles(posX,posY){
+  let circleD = { //circle dimensions
+    x: (windowWidth/8) - 80 + posX,
+    y: (windowHeight/4) - 80 + posY,
+  };
+  for (let i = 0; i<20; i++){ //repeating circle to make a design
+    stroke(100);
+    fill(random(255),random(255),random(255),100);
+    ellipse(circleD.x,circleD.y,random(200),nums[i]+100);
+  }
 }
 
-function spawn(){
-  for (let i= 0; i<width; i ++){
-    for (let j = 0; j< height;j++){}
-      fill(random(255),random(255),random(255));
-      ellipse(mouseX,mouseY,random(100),random(100));
+function fillCircle(){
+
+  let xIncr = windowWidth/8;   //x position shift increment
+  let yIncr = windowHeight/4;  //y position shift increment
+
+  for (let i = 0; i<8; i++){
+    for (let j = 0; j<4; j++){
+      circles(xIncr*i,yIncr*j);
     }
   }
+}
