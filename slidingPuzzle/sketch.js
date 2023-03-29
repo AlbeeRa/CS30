016bb22473cd370df24222210069bd402a5e3497
Albee <ar.senpai03@gmail.com>
2023-03-29 15:10:57 -0600

    gg

--- slidingPuzzle/sketch.js
+++ slidingPuzzle/sketch.js
@@ -18,7 +18,11 @@ function setup() {
 
   for (let i = 0; i < COLS; i ++){
     for (let j = 0; j < ROWS; j++){
-      let img = createImage(w,h);
+      let x = i*w;
+      let y = j*h;
+      let img = createImg(w,h);
+      img.copy(bebop,x,y,w,h,0,0,w,h); /// copy(srcImage,sx,sy,sw,sh,nx,ny,nw,nh) [s = source, n = new]
+      let title = new Tile
     }
   }
 }
