30b12ec0be5348855544174fa37b857458e84692
AlbeeRa <ar.senpai03@gmail.com>
2023-04-04 10:22:59 -0600

    g

--- slidingPuzzle/sketch.js
+++ slidingPuzzle/sketch.js
@@ -4,31 +4,15 @@
 
 
 let bebop;
-let tile = [];
-let ROWS = 4;
+let pieces = [];
 let COLS = 4;
+let ROWS = 4;
 let w,h;
-let board = [];
 function preload(){
   bebop = loadImage("bebop.png");
 }
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  let w = windowWidth/ROWS;
-  let h = windowHeight/COLS;
+function setup(){
+  createCanvas(400,400);
 
-  for (let i = 0; i < COLS; i ++){
-    for (let j = 0; j < ROWS; j++){
-      let x = i*w;
-      let y = j*h;
-      let img = createImg(w,h);
-      img.copy(bebop,x,y,w,h,0,0,w,h); /// copy(srcImage,sx,sy,sw,sh,nx,ny,nw,nh) [s = source, n = new]
-      let index = i+j*COLS;
-      let tile = new Tile (index,img);
-    }
-  }
 }
 
-function draw() {
-  image(bebop,0,0);
-}
